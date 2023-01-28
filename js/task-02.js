b910c21bfe6ad28ac79f03a87f70623e21bf2a16
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-28 20:56:38 +0200

    6

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrLi = []
+
+ingredients.forEach(ingredient => {
+  const ingridientLi = document.createElement("li");
+  ingridientLi.textContent = ingredient;
+  ingridientLi.className = "item";
+  ingridientLi.classList.add('diablo');
+arrLi.push(ingridientLi);
+  
+})
+
+console.log(arrLi);
+document.querySelector("ul").append(...arrLi);
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,5 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
