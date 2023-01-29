2c4cbe5c828c25cb6442c6e5e8099f838a9faf1e
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-29 19:26:04 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,8 +1,6 @@
 let counterValue = 0;
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
-
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-
 const counter = document.querySelector("#value");
 
 decrementBtn.addEventListener("click", () => {
@@ -13,5 +11,4 @@ decrementBtn.addEventListener("click", () => {
 incrementBtn.addEventListener("click", () => {
     counterValue += 1;
     counter.textContent = counterValue;
-
 });
