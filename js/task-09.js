494a2ce2fc97eb6c80f90dba5eff708618013a40
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-30 20:47:07 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  btn: document.querySelector("button.change-color"),
+  output: document.querySelector("span.color"),
+};
+refs.btn.addEventListener("click", onBtnClick);
+function onBtnClick(event) {
+  // const color = getRandomHexColor();
+  // console.log(color);
+  
+refs.output.textContent = `${color}`;
+document.body.style.backgroundColor = `${color}`;
+
+}
