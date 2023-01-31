d7efecc43f7309e991e6713f3fd961bb5e5f16a5
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-31 19:49:06 +0200

    8

--- js/task-08.js
+++ js/task-08.js
@@ -6,18 +6,14 @@ refs.form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
+  const {elements: { email, password },} = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     return alert('Все поля должны быть заполнены!');
   }
-
     // console.log(`email: ${email.value}, Password: ${password.value}`);
     console.log(event.currentTarget.elements.email.value);
     console.log(event.currentTarget.elements.password.value);
 
-    
   event.currentTarget.reset();
 }
