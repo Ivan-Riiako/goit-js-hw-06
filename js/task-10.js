cf0c110d09d8681405b81db14c69cb07848933be
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-31 22:17:40 +0200

    7

--- js/task-10.js
+++ js/task-10.js
@@ -9,6 +9,7 @@ const refs = {
 };
 refs.btnCreateBoxes.addEventListener("click", createBoxes);
 refs.btnDestroyBoxes.addEventListener("click", destroyBoxes);
+// refs.input.addEventListener("input", createBoxes);
 
 
 function createBoxes(amount) {
@@ -16,7 +17,7 @@ function createBoxes(amount) {
   const amountBoxes = [];
   for (let i = 0; i < amount; i += 1) {
     const box = document.createElement("div");
-    let boxSize = 30 + i * 10;
+    let boxSize = 30 + (i * 10);
     box.style.height = `${boxSize}px`;
     box.style.width = `${boxSize}px`;
     box.style.backgroundColor = getRandomHexColor();
