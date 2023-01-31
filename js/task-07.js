d6c07404e85b2e35be38135138debb816ad79dee
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-01-31 19:44:35 +0200

    6

--- js/task-07.js
+++ js/task-07.js
@@ -3,21 +3,31 @@ const refs = {
   spanText: document.querySelector("span#text"),
 };
 refs.input.addEventListener("input", onImputChange);
- let inputCurrentValue = refs.input.value;
-refs.spanText.style.fontSize='16px';
+// Var 1
+refs.input.value = 16;
+function onImputChange(event) {
+  let changeValue = event.currentTarget.value;
+  refs.spanText.style.fontSize = `${changeValue}px`;
+}
+// Var 2
+//  let inputCurrentValue = refs.input.value;
+// refs.spanText.style.fontSize='16px';
 
   
-let spanTextFontSize = Number(refs.spanText.style.fontSize.replace("px", ""));
+// let spanTextFontSize = Number(refs.spanText.style.fontSize.replace("px", ""));
+// // console.log(spanTextFontSize);
 
-function onImputChange(event) {
-  const changeValue = event.currentTarget.value;
 
-  if (changeValue > inputCurrentValue) {
-    spanTextFontSize += 1;
-    refs.spanText.style.fontSize = `${spanTextFontSize}px`;
-  } else if (spanTextFontSize > 0 && changeValue < inputCurrentValue) {
-    spanTextFontSize -= 1;
-    refs.spanText.style.fontSize = `${spanTextFontSize}px`;
-  }
-    inputCurrentValue = changeValue;
-}
+// function onImputChange(event) {
+//   const changeValue = event.currentTarget.value;
+
+//   if (changeValue > inputCurrentValue) {
+//     spanTextFontSize += 1;
+//     refs.spanText.style.fontSize = `${spanTextFontSize}px`;
+//   } else if (spanTextFontSize > 0 && changeValue < inputCurrentValue) {
+//     spanTextFontSize -= 1;
+//     refs.spanText.style.fontSize = `${spanTextFontSize}px`;
+//   }
+//     inputCurrentValue = changeValue;
+//     // console.log(spanTextFontSize);
+// }
